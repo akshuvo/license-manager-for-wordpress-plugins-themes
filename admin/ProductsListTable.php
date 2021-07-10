0d1de682168e48dba023f5723509ba85f85016b4
Al-amintpi <alamincmt7418@gmail.com>
2021-07-10 17:39:31 +0600

    product table create

--- admin/ProductsListTable.php
+++ admin/ProductsListTable.php
@@ -1,2 +1,64 @@
 <?php 
-// Write your class here
+
+namespace admin;
+  
+
+if( !class_exists('WP_List_Table') ){
+	require_once ABSPATH .'wp-admin/includes/class-wp-list-table.php';
+}
+
+
+/**
+ *  
+ * Product List Table Class
+ * 
+*/
+
+
+class ProductsListTable extends \WP_List_Table{
+
+	function __construct(){
+		parent::__construct([
+			'singular' => "product",
+			'plural'   => "products",
+			'ajax'     => false
+		]);
+	}
+
+	// number of column
+	public function get_columns(){
+		return [
+			'cb'     => "<input type='checkbox'/>",
+			'name'   => __('Name','Name'),
+			'slug'   => __('Slug','Slug'),
+			'dated'  => __('Date','Date')
+		];
+	}
+
+
+	protected function column_default($item, $column_name){
+		switch ($column_name) {
+			case 'value':
+				# code...
+				break;
+			
+			default:
+				return isset($item->$column_name) ? $item->$column_name : '';
+		}
+	}
+
+	public function prepare_items(){
+		$per_page = 20; 
+		$column = $this->get_columns();
+		$hidden = [];
+		$sortable = $this->get_sortable_columns();
+
+		$this->_column_headers = [$column, $hidden, $sortable];
+		$this->items = wp_product(); //Fetch All Product
+		$this->set_pagination_args([
+			'total_items' =>product_count(),
+            'per_page'    =>$per_page,
+		]);
+	}
+
+}
--- admin/templates/products/list.php
+++ admin/templates/products/list.php
@@ -10,11 +10,23 @@ $products = $wpdb->get_results( $query, ARRAY_A );
 
     <a href="<?php echo admin_url( 'admin.php?page=license-manager-wppt&action=new' ); ?>" class="page-title-action"><?php _e( 'Add New License', 'lmfwppt' ); ?></a>
 
-    <div class="lmfwppt-ul">
+   <!--  <div class="lmfwppt-ul">
         <?php foreach ( $products as $key => $data ) : ?>
             <div class="lmfwppt-li">
                 <a href="<?php echo admin_url( 'admin.php?page=license-manager-wppt' ); ?>&action=edit&id=<?php esc_attr_e( $data['id'] ); ?>"><h4><?php _e( $data['name'] ); ?></h4></a>
             </div>
         <?php endforeach;?>
-    </div>
+    </div> -->
+ 
+    <form action="" method="post">
+    	<?php 
+    		$table = new \admin\ProductsListTable();
+    		$table->prepare_items();
+    		$table->display();
+
+
+    	?>
+    </form>
+
 </div>
+
